01357a4b997876d27e8ce245e14f7e6d287c55ca
AngelTroncoso <157044866+AngelTroncoso@users.noreply.github.com>
2024-07-11 19:09:37 -0400

    quinto avance

---
+++ assets/javascript/doc.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("p").dblclick(function(){
+    $(this).hide("slow");
+    });
+    });
+
+
+$("p").click(function(){
+    $(this).css({
+    "color": "red",
+    "font-size": "2em"
+    });
+    });
+    
